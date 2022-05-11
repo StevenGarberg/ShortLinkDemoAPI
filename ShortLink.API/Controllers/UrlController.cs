7edd9690d287858387c9eb702ee51cbc2bbd3d5b
Steven Garberg <steven.garberg.jr@gmail.com>
2022-05-11 16:45:31 -0400

    Added scaffolding for URL Controller

---
+++ ShortLink.API/Controllers/UrlController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ShortLink.API.Controllers;
+
+[ApiController]
+public class UrlController : ControllerBase
+{
+    [HttpPost("encode")]
+    public Task<IActionResult> Encode()
+    {
+        throw new NotImplementedException();
+    }
+    
+    [HttpPost("decode")]
+    public Task<IActionResult> Decode()
+    {
+        throw new NotImplementedException();
+    }
+}
