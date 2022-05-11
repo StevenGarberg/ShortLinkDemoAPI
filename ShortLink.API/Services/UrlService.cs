55f490751b7afa490231ba6d6f0c2b8838f13767
Steven Garberg <steven.garberg.jr@gmail.com>
2022-05-11 17:21:53 -0400

    Cleaning up service interface

--- ShortLink.API/Services/UrlService.cs
+++ ShortLink.API/Services/UrlService.cs
@@ -1,21 +1,33 @@
 ﻿using ShortLink.API.Models.Requests;
+using ShortLink.API.Models.Responses;
 
 namespace ShortLink.API.Services;
 
 public interface IUrlService
 {
-    Task<string> Encode(UrlRequest request);
-    Task<string> Decode(UrlRequest request);
+    /// <summary>
+    /// Encodes a user supplied URL
+    /// </summary>
+    /// <param name="request">URL Request</param>
+    /// <returns>URL Response</returns>
+    Task<UrlResponse> Encode(UrlRequest request);
+    
+    /// <summary>
+    /// Decodes a user supplied URL, if it exists in the data store
+    /// </summary>
+    /// <param name="request">URL Request</param>
+    /// <returns>URL Response</returns>
+    Task<UrlResponse> Decode(UrlRequest request);
 }
 
 public class UrlService : IUrlService
 {
-    public Task<string> Encode(UrlRequest request)
+    public Task<UrlResponse> Encode(UrlRequest request)
     {
         throw new NotImplementedException();
     }
 
-    public Task<string> Decode(UrlRequest request)
+    public Task<UrlResponse> Decode(UrlRequest request)
     {
         throw new NotImplementedException();
     }
