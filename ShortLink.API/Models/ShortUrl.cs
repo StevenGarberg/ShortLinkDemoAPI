5fdfeef4a64eed4e8ca3e289666c08bc765585bb
Steven Garberg <steven.garberg.jr@gmail.com>
2022-05-11 17:26:46 -0400

    Added ShortUrl model

---
+++ ShortLink.API/Models/ShortUrl.cs
@@ -0,0 +1,15 @@
+﻿namespace ShortLink.API.Models;
+
+public class ShortUrl
+{
+    public string OriginalUrl { get; }
+    public string EncodedUrl { get; }
+    public DateTime CreatedAt { get; }
+
+    public ShortUrl(string originalUrl, string encodedUrl)
+    {
+        OriginalUrl = originalUrl;
+        EncodedUrl = encodedUrl;
+        CreatedAt = DateTime.UtcNow;
+    }
+}
