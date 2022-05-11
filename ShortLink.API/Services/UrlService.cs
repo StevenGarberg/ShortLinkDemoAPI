4188e29f4d55af93566a9d757af87bcc218540ad
Steven Garberg <steven.garberg.jr@gmail.com>
2022-05-11 18:51:11 -0400

    Implemented URL service

--- ShortLink.API/Services/UrlService.cs
+++ ShortLink.API/Services/UrlService.cs
@@ -1,4 +1,6 @@
-﻿using ShortLink.API.Models.Requests;
+﻿using ShortLink.API.Models;
+using ShortLink.API.Models.Exceptions;
+using ShortLink.API.Models.Requests;
 using ShortLink.API.Models.Responses;
 using ShortLink.API.Repositories;
 
@@ -30,13 +32,42 @@ public class UrlService : IUrlService
         _urlRepository = urlRepository;
     }
     
-    public Task<UrlResponse> Encode(UrlRequest request)
+    public async Task<UrlResponse> Encode(UrlRequest request)
     {
-        throw new NotImplementedException();
+        string code = null;
+        ShortUrl shortUrl = null;
+        
+        for (var i = 0; i < 10000; i++)
+        {
+            code = await _urlRepository.GenerateCode();
+            shortUrl = await _urlRepository.GetByCode(code);
+            if (shortUrl == null)
+                break;
+        }
+        
+        if (shortUrl != null) throw new CodeGenerationException();
+        
+        await _urlRepository.CreateShortUrl(request.Url, code);
+        
+        return new UrlResponse($"https://{Constants.BaseUrl}{code}");
     }
 
-    public Task<UrlResponse> Decode(UrlRequest request)
+    public async Task<UrlResponse> Decode(UrlRequest request)
     {
-        throw new NotImplementedException();
+        string code;
+        try
+        {
+            code = request.Url.Split(Constants.BaseUrl)[1];
+        }
+        catch
+        {
+            throw new UrlFormatException();
+        }
+        
+        var shortUrl = await _urlRepository.GetByCode(code);
+        
+        if (shortUrl == null) throw new NotFoundException();
+        
+        return new UrlResponse(shortUrl.OriginalUrl);
     }
 }
