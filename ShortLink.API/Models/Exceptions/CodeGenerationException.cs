b76aa381b803fc091bc058c4032928600eafb59c
Steven Garberg <steven.garberg.jr@gmail.com>
2022-05-11 18:49:29 -0400

    Added API exceptions

---
+++ ShortLink.API/Models/Exceptions/CodeGenerationException.cs
@@ -0,0 +1,9 @@
+﻿namespace ShortLink.API.Models.Exceptions;
+
+public class CodeGenerationException : ApiException
+{
+    public CodeGenerationException()
+        : base("An error occurred while encoding the supplied URL.", StatusCodes.Status422UnprocessableEntity)
+    {
+    }
+}
---
+++ ShortLink.API/Models/Exceptions/UrlFormatException.cs
@@ -0,0 +1,9 @@
+﻿namespace ShortLink.API.Models.Exceptions;
+
+public class UrlFormatException : ApiException
+{
+    public UrlFormatException()
+        : base("The supplied URL was not formatted correctly.", StatusCodes.Status422UnprocessableEntity)
+    {
+    }
+}
